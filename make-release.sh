ffc7bd7542281c17737bfe14c61227c3a4f4fa47
Andreas Lorensen <andlo@outlook.dk>
2018-11-20 20:50:12 +0000

    ...

--- make-release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# Create a distributable archive of the current version
-
-../makeself-2.4.0/makeself.sh --current ./theia-for-mycroft theia-for-mycroft.sh "THEIA IDE for Mycroft" ./auto_run.sh
