d4f12b2837ee49f5396ba55e1ee3e6f2c553ae8e
Andreas Lorensen <andlo@outlook.dk>
2018-11-21 16:22:46 +0000

    ...

--- theia_run.sh
+++ theia_run.sh
@@ -5,6 +5,16 @@ cd $1
 #export LOCAL_GIT_DIRECTORY="/usr"
 #export GIT_EXEC_PATH="/usr/lib/git-core"
 
+## If on Mark_1 enter venv
+if [ -f /opt/venvs/mycroft-core/bin/activate ]; then
+    source /opt/venvs/mycroft-core/bin/activate
+fi
+
+## If on picroft enter venv
+if [ -f /home/pi/mycroft-core/venv-activate.sh ]; then
+    /home/pi/mycroft-core/venv-activate.sh
+fi
+
 ## setup and load nvm
 export NVM_DIR="$(pwd)/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >/dev/null 2>/dev/null # This loads nvm
