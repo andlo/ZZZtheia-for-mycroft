29ac5eb2747a01aa44cd8296e1099938fa4a964c
Andreas Lorensen <andlo@outlook.dk>
2018-11-26 21:51:30 +0000

    made git workig, and mad better compiling of git

--- theia_install.sh
+++ theia_install.sh
@@ -17,6 +17,7 @@ npm install -g yarn
 echo "Building theia..."
 yarn
 yarn theia build
+rm -r ./node_modules/electron*
 
 echo "Building thiea OK"
 
@@ -28,9 +29,35 @@ git clone git://git.kernel.org/pub/scm/git/git.git >/dev/null 2>/dev/null
 cd git
 echo "Compiling git..."
 make configure >/dev/null 2>/dev/null
-./configure --prefix=$(pwd)/../node_modules/dugite/git >/dev/null 2>/dev/null
-make >/dev/null 2>/dev/null
+
+CC='gcc' \
+  CFLAGS='-Wall -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -U_FORTIFY_SOURCE' \
+  LDFLAGS='-Wl,-Bsymbolic-functions -Wl,-z,relro' \
+  ./configure \
+  --prefix=$(pwd)/../node_modules/dugite/git
+
 echo "Installing git...into node_modules/dugite/git"
-make install >/dev/null 2>/dev/null
+
+DESTDIR="$(pwd)/../node_modules/dugite/git" \
+    NO_PERL=1 \
+    NO_TCLTK=1 \
+    NO_GETTEXT=1 \
+    NO_INSTALL_HARDLINKS=1 \
+    NO_R_TO_GCC_LINKER=1 \
+    make strip install >/dev/null 2>/dev/null
+
+
+echo "-- Removing server-side programs"
+rm "$(pwd)/../node_modules/dugite/git/bin/git-cvsserver"
+rm "$(pwd)/../node_modules/dugite/git/bin/git-receive-pack"
+rm "$(pwd)/../node_modules/dugite/git/bin/git-upload-archive"
+rm "$(pwd)/../node_modules/dugite/git/bin/git-upload-pack"
+rm "$(pwd)/../node_modules/dugite/git/bin/git-shell"
+
+echo "-- Removing unsupported features"
+rm "$(pwd)/../node_modules/dugite/libexec/git-core/git-svn"
+rm "$(pwd)/../node_modules/dugite/libexec/git-core/git-remote-testsvn"
+rm "$(pwd)/../node_modules/dugite/libexec/git-core/git-p4"
+
 cd ..
 rm -r git/ >/dev/null 2>/dev/null
