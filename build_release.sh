4cee337dd8ee80dc594e4537ee101b2b96f87a11
andlo <andlo@outlook.dk>
2019-08-25 21:11:49 +0100

    test

--- build_release.sh
+++ build_release.sh
@@ -8,26 +8,22 @@ mkdir $NVM_DIR
 cp theia_run.sh $RELDIR
 cp package.json $RELDIR
 
-cd release
+cd $RELDIR
 
 echo "Installing nvm..."
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 curl -s -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash >/dev/null 2>/dev/null
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  >/dev/null 2>/dev/null # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  >/dev/null 2>/dev/null # This loads nvm bash_completion
 
 export NODE_OPTIONS=--max_old_space_size=1024
 
 echo "Installing node..."
-#nvm install 8
 nvm install 10 >/dev/null 2>/dev/null 
 
 echo "Installing yarn..."
 npm install -g yarn >/dev/null 2>/dev/null 
 
 echo "Building theia..."
-
 yarn
 #yarn --pure-lockfile
 yarn theia build
@@ -41,7 +37,7 @@ yarn autoclean --force
 rm -rf ./node_modules/electron*
 yarn cache clean
 
-echo "Downloading Mycroft VS-code plugin..."
+echo "Downloading Microsoft VS-code plugin..."
 mkdir vscode-plugins
 cd vscode-plugins
 wget https://github.com/$(wget https://github.com/Microsoft/vscode-python/releases/latest -O- | egrep '/.*/.*/.*vsix' -o) >/dev/null 2>/dev/null
@@ -49,5 +45,9 @@ cd ..
 cd ..
 echo "Packing theia...."
 rm -rf $NVM_DIR
-tar -czvf theiaide-picroft.tgz release/*
+cd $RELDIR
+tar -czvf theiaide-picroft.tgz *
+mv tar -czvf theia-mycroft-$(uname -m)-$(date +%Y%m%d).tgz ..
+cd ..
+rm -rf $RELDIR
 echo "Building theia...OK"
