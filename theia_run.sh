c2b3280ebe2bb958690842344273a6440e2e1e21
andlo <andlo@outlook.dk>
2020-01-10 22:20:26 +0000

    fix build  on non Pi's

--- theia_run.sh
+++ theia_run.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$(pwd)/nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >/dev/null 2>/dev/null # This loads nvm bash_completion
 
 ## set the VS code plugins dir
-export THEIA_DEFAULT_PLUGINS=local-dir://$1/vscode-plugins
+export THEIA_DEFAULT_PLUGINS=local-dir://$(pwd)/vscode-plugins
 
 ## run theia-ide
 yarn theia start $1 --startup-timeout -1 --hostname 0.0.0.0 --port 3000 >/dev/null 2>/dev/null
