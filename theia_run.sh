38c43e40e4a7e245c01d219581047bf38e4a6daf
andlo <andlo@outlook.dk>
2020-04-04 18:41:09 +0100

    update to theia latest

--- theia_run.sh
+++ theia_run.sh
@@ -7,13 +7,13 @@ export NVM_DIR="$(pwd)/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >/dev/null 2>/dev/null # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >/dev/null 2>/dev/null # This loads nvm bash_completion
 
-if ! [ -f $workspace/.theia ]; then
-    mkdir  $workspace/.theia
-fi
+#f ! [ -f $workspace/.theia ]; then
+#    mkdir  $workspace/.theia
+#fi
 
-if ! [ -f $workspace/.theia/launch.json ]; then
-    cp $(pwd)/launch.json $workspace/.theia/
-fi
+#if ! [ -f $workspace/.theia/launch.json ]; then
+#    cp $(pwd)/launch.json $workspace/.theia/
+#fi
 
 ## set the VS code plugins dir
 export THEIA_DEFAULT_PLUGINS=local-dir://$(pwd)/plugins
